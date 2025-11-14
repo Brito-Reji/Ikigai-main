40d2a252a2500770879a1237e24f7cedde25f36b
Brito-Reji <britoreji03@gmail.com>
2025-11-14 17:19:22 +0530

    added linters

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,9 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import pluginReact from 'eslint-plugin-react'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Vite/React 18 doesn't need React import
+    },
+  },
+])
