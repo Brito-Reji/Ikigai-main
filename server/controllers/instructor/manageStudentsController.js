f12eabffd9eafb9067f263468f7c0a09de81e5e2
Brito-Reji <britoreji03@gmail.com>
2025-11-17 18:33:37 +0530

    some changes are made

---
+++ server/controllers/instructor/manageStudentsController.js
@@ -0,0 +1,4 @@
+// import asyncHandler from 'express-async-handler'
+// export const getStudents = asyncHandler(async (req, res) => {
+    
+// })
---
+++ server/controllers/students/courseController.js
@@ -0,0 +1,7 @@
+// import asyncHandler from 'express-async-handler'
+// import { Course } from '../../models/Course.js'
+ 
+// export const getCourses = asyncHandler(async (req, res) => {
+//     let course = await Course.find()
+//     console.log(course)
+// })
