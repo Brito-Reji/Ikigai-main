387c1c0931a87d03de4c46d0345fd06b301283ca
Brito-Reji <britoreji03@gmail.com>
2025-12-11 15:38:23 +0530

    fix : fixed th eimport error

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import useUser from "@/hooks/useUser.js";
+import { useNavigate } from "react-router-dom";
 
 function Layout() {
   const [menuOpen, setMenuOpen] = useState(false);  
+  const navigate = useNavigate()
   let user = useUser()
    useEffect(() => {
-        if(!user && user.role !== "student") {
+        if(!user && user?.role !== "student") {
       navigate("/")
     }
     },[])
