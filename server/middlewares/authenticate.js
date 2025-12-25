c734f3cff72e35b04acf9f8894a626999f97126c
Brito-Reji <britoreji03@gmail.com>
2025-12-25 15:31:01 +0530

    refactor: Dynamically select user model and simplify error handling in authentication middleware.

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -3,48 +3,50 @@ import asyncHandler from "express-async-handler";
 import {User} from "../models/User.js";
 import { Instructor } from "../models/Instructor.js";
 
+const MODELS = {
+  student: User,
+  admin: User,
+  instructor: Instructor,
+};
+
 const authenticate = asyncHandler(async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  // Allow guest access (no token)
   if (!authHeader) {
     req.user = { role: "guest" };
     return next();
   }
 
   const token = authHeader.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+  const Model = MODELS[decoded.role];
 
-   let user;
-
-    if (decoded.role === "student" || decoded.role === "admin") {
-      user = await User.findById(decoded.id).select("-password");
-    } else if (decoded.role === "instructor") {
-      user = await Instructor.findById(decoded.id).select("-password");
-    }
-    if (!user) {
-      res.status(401);
-      throw new Error("User not found");
-    }
-
-    if (user.isBlocked) {
-      res.status(403);
-      throw new Error("Account is blocked");
-    }
-
-    req.user = {
-      id: user._id,
-      email: user.email,
-      role: user.role,
-    };
-
-    next();
-  } catch (error) {
+  if (!Model) {
     res.status(401);
-    throw new Error("Invalid or expired token",error);
+    throw new Error("Invalid role");
   }
+
+  const user = await Model.findById(decoded.id).select("-password");
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (user.isBlocked) {
+    res.status(403);
+    throw new Error("Account is blocked");
+  }
+
+  req.user = {
+    id: user._id,
+    email: user.email,
+    role: decoded.role,
+  };
+
+  next();
 });
+
 
 export default authenticate;
