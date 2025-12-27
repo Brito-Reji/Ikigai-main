38f34732c7a92b9be178b78ca055680a3aac6019
Brito-Reji <britoreji03@gmail.com>
2025-12-27 16:22:23 +0530

    feat: include lessons within public course chapters

--- server/services/public/publicChapterService.js
+++ server/services/public/publicChapterService.js
@@ -1,4 +1,5 @@
 import { Chapter } from "../../models/Chapter.js";
+import { Lesson } from "../../models/Lesson.js";
 
 // GET PUBLIC COURSE CHAPTERS
 export const getPublicCourseChaptersService = async (courseId) => {
@@ -8,5 +9,26 @@ export const getPublicCourseChaptersService = async (courseId) => {
     })
         .sort({ order: 1 });
 
-    return chapters;
+    const chapterIds = chapters.map(ch => ch._id);
+    
+    const lessons = await Lesson.find({
+        chapter: { $in: chapterIds }
+    }).sort({ order: 1 });
+
+    const lessonsByChapter = lessons.reduce((acc, lesson) => {
+        const chapterId = lesson.chapter.toString();
+        if (!acc[chapterId]) {
+            acc[chapterId] = [];
+        }
+        acc[chapterId].push(lesson);
+        return acc;
+    }, {});
+
+    const chaptersWithLessons = chapters.map(chapter => {
+        const chapterObj = chapter.toObject();
+        chapterObj.lessons = lessonsByChapter[chapter._id.toString()] || [];
+        return chapterObj;
+    });
+
+    return chaptersWithLessons;
 };
